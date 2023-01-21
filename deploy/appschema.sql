f6437ace49611ef09801f40313c46328587877eb
jthomas <joel.thomas_1@telensa.com>
2023-01-21 14:28:40 +0000

    new student tables added

--- deploy/appschema.sql
+++ deploy/appschema.sql
@@ -4,9 +4,9 @@ BEGIN;
 
 -- XXX Add DDLs here.
 
---create table tab_1 (name character(20),age integer);
---create table tab_2 (name character(20),age integer);
-create table tab_4 (name character(20),age integer);
+create table tab_student(name character(20),age integer);
+create table tab_class (name character(20),age integer);
+create table tab_location (name character(20),age integer);
 commit;
 
 
