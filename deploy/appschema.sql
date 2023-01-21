a70ca4296078d495315c01f29bb1c83f228201a9
jthomas <joel.thomas_1@telensa.com>
2023-01-21 14:44:59 +0000

    last change

--- deploy/appschema.sql
+++ deploy/appschema.sql
@@ -4,8 +4,6 @@ BEGIN;
 
 -- XXX Add DDLs here.
 
-create table tab_student(name character(20),age integer);
-create table tab_class (name character(20),age integer);
 create table tab_location (name character(20),age integer);
 commit;
 
