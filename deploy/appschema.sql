73d342be3a2da4b1a58d0970fa25a2e94d7f89b9
jthomas <joel.thomas_1@telensa.com>
2023-01-21 13:20:39 +0000

    new table added

--- deploy/appschema.sql
+++ deploy/appschema.sql
@@ -4,9 +4,9 @@ BEGIN;
 
 -- XXX Add DDLs here.
 
-create table tab_1 (name character(20),age integer);
-create table tab_2 (name character(20),age integer);
-create table tab_3 (name character(20),age integer);
+--create table tab_1 (name character(20),age integer);
+--create table tab_2 (name character(20),age integer);
+create table tab_4 (name character(20),age integer);
 commit;
 
 
