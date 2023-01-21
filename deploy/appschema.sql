00f245b97417a67079714dcfcb23a05197d907ca
jthomas <joel.thomas_1@telensa.com>
2023-01-21 14:54:31 +0000

    employee table created

--- deploy/appschema.sql
+++ deploy/appschema.sql
@@ -6,7 +6,7 @@ BEGIN;
 
 --create table tab_1 (name character(20),age integer);
 --create table tab_2 (name character(20),age integer);
-create table tab_4 (name character(20),age integer);
+create table tab_employee (name character(20),age integer);
 commit;
 
 
